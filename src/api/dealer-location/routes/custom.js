734bc93a2feafda5d1d1478ddae68265e7ab8973
vivinkv <vivin@spiderworks.in>
2025-07-07 18:00:12 +0530

    update

--- src/api/dealer-location/routes/custom.js
+++ src/api/dealer-location/routes/custom.js
@@ -2,6 +2,16 @@ module.exports = {
   routes: [
     {
       method: "GET",
+      handler: "custom.List",
+      path: "/dealer-location",
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+    }, ,
+    {
+      method: "GET",
       handler: "custom.getBySlug",
       path: "/dealer-location/:slug",
       config: {
